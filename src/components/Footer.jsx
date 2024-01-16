442d412bdf1307a1efd6ecb62aa0b0bcc61fb627
MAmmad-byte <83021476+MAmmad-byte@users.noreply.github.com>
2024-01-16 12:45:51 +0500

    Update Footer.jsx
    text editor removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import PageLayout from './PageLayout'
 import { Box, Text } from '@chakra-ui/react'
-import TextEditor from './textEditor'
 export default function Footer() {
   return (
     <Box alignSelf={"flex-end"} width={"100%"} padding="10px 0" background="teal" color="#fff" >
        
     <PageLayout>
     <Text textAlign="center">Copy Right 2024 | All Right Reserved</Text>
-    {/* <TextEditor placeholder={"Write Sometibng"} /> */}
+   
   </PageLayout>
       </Box>
   )
