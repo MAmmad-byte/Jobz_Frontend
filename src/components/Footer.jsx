15b330ed5624ba25cc2a453d9e47be8367d0513c
Muhammad Amamd <muhammadammad146@gmail.com>
2024-01-30 21:10:31 +0500

    Icon updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import PageLayout from './PageLayout'
-import { Box, Text } from '@chakra-ui/react'
+import React from "react";
+import PageLayout from "./PageLayout";
+import { Box, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 export default function Footer() {
   return (
-    <Box alignSelf={"flex-end"} width={"100%"} padding="10px 0" background="teal" color="#fff" >
-       
-    <PageLayout>
-    <Text textAlign="center">Copy Right 2024 | All Right Reserved</Text>
-   
-  </PageLayout>
-      </Box>
-  )
+    <Box
+      alignSelf={"flex-end"}
+      width={"100%"}
+      padding="10px 0"
+      background="teal"
+      color="#fff"
+    >
+      <PageLayout>
+        <Text textAlign="center">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/muhammad-ammad-a8b7a8267?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+          >
+            Created by: Muhammad Ammad
+          </a>
+        </Text>
+      </PageLayout>
+    </Box>
+  );
 }
